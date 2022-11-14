7a3fe65f83ac37cead9b48db76382739c0b337f4
AndreyR <rubanoff.and@yandex.by>
2022-11-14 12:16:43 +0300

    Последня версия
    Программа для шифрования и дешифрования AES и Кузнечек

---
+++ View/CipherView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab2._1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для CipherView.xaml
+    /// </summary>
+    public partial class CipherView : UserControl
+    {
+        public CipherView()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
